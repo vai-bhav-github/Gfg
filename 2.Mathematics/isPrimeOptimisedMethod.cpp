19563074974b300c8bbbfd1ea31e829f33028f1e
vai-bhav-github <109478639+vai-bhav-github@users.noreply.github.com>
2022-07-23 12:28:30 +0530

    Create isPrimeOptimisedMethod.cpp

---
+++ 2.Mathematics/isPrimeOptimisedMethod.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+bool isPrime(int n)
+{
+    if(n==1)
+        return false;
+    if(n==2 || n==3)
+        return true;
+    if(n%2==0 || n%3==0)
+        return false;
+    for(int i=5; i*i<=n; i=i+6)
+    {
+        if(n%i==0 || n%(i+2)==0)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+   int n;
+   cin>>n;
+   if(isPrime(n)){
+    cout<<"true";
+   } else
+   cout<<"false";
+   return 0;
+
+
+
+}
