2c5eb33763bd2ec3af9427fc2cd11def8e802309
vai-bhav-github <109478639+vai-bhav-github@users.noreply.github.com>
2022-07-24 11:54:19 +0530

    Create absouleNumber.cpp

---
+++ 2.Mathematics/absouleNumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    long int i;
+    cin>>i;
+    if(i>0){
+        cout<<i<<endl;
+    } else {
+        cout<<-1*i<<endl;
+    }
+
+     return 0;
+}
